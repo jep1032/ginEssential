ecf590e3c104bc618c02cd18619153a6df3618dd
jocker <jocker@git.com>
2023-03-12 15:33:00 +0800

    登录验证

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"ginessential/common"
 	"github.com/gin-gonic/gin"
 )
@@ -10,11 +9,6 @@ func main() {
 	_ = common.InitDb()
 
 	r := gin.Default()
-	fmt.Println("master 修改")
-	fmt.Println("master 在网页修改 修改")
-	fmt.Println("master 在goland 修改")
-	fmt.Println("master 在goland 修改2")
-	fmt.Println("master 在goland 修改3")
 	r = CollectRoute(r)
 	panic(r.Run())
 }
