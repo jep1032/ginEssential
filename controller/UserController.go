af1e587b2f4f6973a59161aee40ddb0d85a5b5fe
jocker <jocker@git.com>
2023-03-12 15:33:43 +0800

    登录验证

--- controller/UserController.go
+++ controller/UserController.go
@@ -4,6 +4,7 @@ import (
 	"ginessential/common"
 	"ginessential/model"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"log"
 	"math/rand"
@@ -52,28 +53,25 @@ func Register(c *gin.Context) {
 	}
 
 	// 创建用户
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密错误"})
+		return
+	}
 	newUser := model.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hasedPassword),
 	}
 	DB.Create(&newUser)
 
 	// 返回结果
-
 	c.JSON(http.StatusOK, gin.H{
-		"msg": "注册成功",
+		"code": 200,
+		"msg":  "注册成功",
 	})
 }
-func isTelephoneExist(db *gorm.DB, telephone string) bool {
-	var user model.User
-	db.Where("telephone = ?", telephone).First(&user)
-	if user.ID != 0 {
-		return true
-	}
-	return false
 
-}
 func RandomString(n int) string {
 	var letters = []byte("ssssssssssss")
 	result := make([]byte, n)
@@ -83,4 +81,65 @@ func RandomString(n int) string {
 		result[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(result)
+}
+
+func Login(c *gin.Context) {
+	DB := common.GetDB()
+	// 获取参数
+	telephone := c.PostForm("telephone")
+	password := c.PostForm("password")
+
+	// 数据验证
+	if len(telephone) != 11 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "手机号必须为11位",
+		})
+		return
+	}
+	if len(password) < 6 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "密码不能少于6位",
+		})
+		return
+	}
+
+	// 判断手机号是否存在
+	var user model.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "用户不存在",
+		})
+		return
+	}
+
+	// 判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "密码错误"})
+		return
+	}
+
+	// 发送token
+	token := "111"
+
+	// 返回结果
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "登录成功",
+	})
+
+}
+
+func isTelephoneExist(db *gorm.DB, telephone string) bool {
+	var user model.User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+	return false
+
 }
